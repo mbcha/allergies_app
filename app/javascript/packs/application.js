f840a58096eb5def274e0134d7be1a3f91c6c973
Bella Cha <cha.maria.belen@gmail.com>
2019-01-10 13:37:04 +0100

    fixed sign up problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,5 @@
 import "bootstrap";
 import { search } from '../components/search';
 import { translate } from '../components/translate';
-import { changeLanguage } from '../components/changeLanguage';
 search();
 translate();
-changeLanguage();
