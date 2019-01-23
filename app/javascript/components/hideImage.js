af14aa727e965e2593334f921f93418fd410a887
Bella Cha <cha.maria.belen@gmail.com>
2019-01-23 14:33:05 +0100

    changed some css

--- app/javascript/components/hideImage.js
+++ app/javascript/components/hideImage.js
@@ -1,11 +1,26 @@
-const toggleSwitch = document.querySelector(".toggle-switch");
-const image = document.querySelector(".allergy-image");
+const toggleSwitches = document.querySelectorAll(".toggle-switch");
+const images = document.querySelectorAll(".allergy-image");
+const names = document.querySelectorAll(".translated-allergy");
 
 function hideImage() {
-  if (toggleSwitch) {
-    toggleSwitch.addEventListener('click', (event) => {
-      console.log("yes")
-      image.classList.toggle('hidden');
+  if (toggleSwitches.length > 1) {
+    toggleSwitches.forEach(toggleSwitch => {
+      toggleSwitch.addEventListener('click', (event) => {
+        names.forEach((name) => {
+          if (toggleSwitch.classList.contains(name.innerText.trim())) {
+            images.forEach(image => {
+              if (image.classList.contains(name.innerText.trim())) {
+                image.classList.toggle('hidden');
+              }
+            })
+          }
+        })
+      })
+    })
+  }
+  else if (toggleSwitches.length == 1) {
+    toggleSwitches[0].addEventListener('click', (event) => {
+      images[0].classList.toggle('hidden');
     })
   }
 }
